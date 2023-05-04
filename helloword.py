b03aa4594f5cf146e9073a6507a1873e756fd4cf
Thiago Machado <thiagogomesmachado@gmail.com>
2023-05-04 10:49:20 -0300

    multi language

--- helloword.py
+++ helloword.py
@@ -1,11 +1,32 @@
 #!/c/Users/z003btsx/AppData/Local/Programs/Python/Python311/python
+"""Hello world multi linguas
 
-print("Hello, World")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
 
-i=2
+Como usar:
+Tenha a variável LANG devidamente configurada ex:
 
-if i==2:
-    print("true")  
+    export LANG=pt_BR
 
-else:
-    print ("false")
+Execução:
+
+    python helloword.py
+    ./helloword.py
+"""
+__version__ = "0.0.1"
+__author__ = "TGM"
+__license__ ="unlicense"
+# dunder - dois underlines, nome sugestivo para facilitar.
+
+import os
+
+current_language =os.getenv("LANG","en_US")[:5]
+msg="Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
